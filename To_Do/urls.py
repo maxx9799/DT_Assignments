954579ded2c440670f286476ade94c0d5da3703c
maxx9799 <59206188+maxx9799@users.noreply.github.com>
2021-08-21 12:57:23 +0530

    Add files via upload

---
+++ To_Do/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="list"),
+    path("update_task/<str:pk>/", views.updateTask, name="upadate_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),
+]
